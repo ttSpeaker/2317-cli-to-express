83294d4ff99b060d03259d086299439256f68df4
salvadorserruya <salvador.serruya@kavak.com>
2023-05-04 21:17:10 -0300

    build command

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import { stockRouter } from "./stockRouter";
 
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/stock/", stockRouter);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("Server running on port 3000");
 });
