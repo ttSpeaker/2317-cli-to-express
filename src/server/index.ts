da2276450a43bda6cb4c36cf197e64844f0935c9
salvadorserruya <salvador.serruya@kavak.com>
2023-06-27 21:25:09 -0300

    autenticacion contrasena y refresh token

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,26 +3,68 @@ import { productRouter } from "./ProductsRouter";
 import { orderRouter } from "./OrdersRouter";
 import { createPrismaClient } from "../repository/prisma";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { prisma } from "../repository/prisma";
+import { isBreakOrContinueStatement } from "typescript";
+import { users } from "@prisma/client";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
-if (!process.env.ACCESS_TOKEN_SECRET) {
+if (!process.env.ACCESS_TOKEN_SECRET || !process.env.REFRESH_TOKEN_SECRET) {
   throw new Error("ACCES TOKEN SECRET NOT PRESENT");
 }
-const secret: string = process.env.ACCESS_TOKEN_SECRET;
+const access_token_secret: string = process.env.ACCESS_TOKEN_SECRET;
+const refresh_token_secret: string = process.env.REFRESH_TOKEN_SECRET;
 app.use(express.json());
-app.post("/login", (req: express.Request, res: express.Response) => {
-  // CHECK LOS DATOS DE LOGIN
-  const { user } = req.body;
-  const accessToken = jwt.sign({ user: user, role: "ADMIN" }, secret, {
-    expiresIn: "1h",
-  });
-  res.json({ access_token: accessToken });
+app.post("/register", async (req: express.Request, res: express.Response) => {
+  const { email, password } = req.body;
+  // validar que el email no exista!
+  const hash = await bcrypt.hash(password, 10);
+  try {
+    const user = await prisma().users.create({
+      data: {
+        email: email,
+        password: hash,
+      },
+    });
+    res.json(user);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
-app.get("/", (req: express.Request, res: express.Response) => {
-  // solo para usuarios logueados
+app.post("/login", async (req: express.Request, res: express.Response) => {
+  const { email, password } = req.body;
+  // validar email y password
+  try {
+    const user = await prisma().users.findUnique({ where: { email: email } });
+    if (user === null) {
+      res.status(404).send("User not found");
+      return;
+    }
+    const result = await bcrypt.compare(password, user.password);
+    if (result) {
+      const accessToken = jwt.sign(
+        { email: email, role: "ADMIN" },
+        access_token_secret,
+        {
+          expiresIn: "1h",
+        }
+      );
+      const refreshToken = jwt.sign({ email: email }, refresh_token_secret, {
+        expiresIn: "72h",
+      });
+      res.json({ access_token: accessToken, refreshToken: refreshToken });
+      return;
+    }
+    res.status(401).send("Invalid password");
+    return;
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 
+app.post("/refresh", async (req: express.Request, res: express.Response) => {
 
   const header = req.headers.authorization;
   if (!header) {
@@ -31,7 +73,54 @@ app.get("/", (req: express.Request, res: express.Response) => {
   }
   const token = header.split(" ")[1];
   try {
-    const data = jwt.verify(token, secret);
+    const data = jwt.verify(token, refresh_token_secret);
+    if (data) {
+      const dataparsed = data as unknown as users;
+
+      const user = await prisma().users.findUnique({
+        where: { email: dataparsed.email },
+      });
+      if (user === null) {
+        res.send(404);
+        return;
+      }
+      const accessToken = jwt.sign(
+        { email: user.email, role: "ADMIN" },
+        access_token_secret,
+        {
+          expiresIn: "1h",
+        }
+      );
+      const refreshToken = jwt.sign({ email: user.email }, refresh_token_secret, {
+        expiresIn: "72h",
+      });
+      res.json({ access_token: accessToken, refreshToken: refreshToken });
+      return;
+    }
+  } catch (err: any) {
+    if (err.name === "TokenExpiredError") {
+      res.status(401).json({ message: "NOT AUTHORIZED: TOKEN EXPIRED" });
+      return;
+    }
+    res.status(401).json({ message: "NOT AUTHORIZED: TOKEN NOT VALID" });
+    return;
+  }
+
+  res.status(401).json({ message: "NOT AUTHORIZED: TOKEN NOT VALID" });
+  return;
+});
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  // solo para usuarios logueados
+
+  const header = req.headers.authorization;
+  if (!header) {
+    res.status(401).json({ message: "NOT AUTHORIZED: TOKEN NOT PRESENT" });
+    return;
+  }
+  const token = header.split(" ")[1];
+  try {
+    const data = jwt.verify(token, access_token_secret);
     if (data) {
       const name = (data as any).user;
       const role = (data as any).role;
